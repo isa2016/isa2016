aa4aa9163e1c76913d53f2d6cf18da9849fc3fbc
isa2016 <uvrnuto@gmail.com>
2017-02-20 14:28:45 +0100

    Izmena restorana od strane menadzera

--- restoran/src/main/java/restoran/kontroleri/MenadzerController.java
+++ restoran/src/main/java/restoran/kontroleri/MenadzerController.java
@@ -2,21 +2,25 @@ package restoran.kontroleri;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import restoran.model.Restoran;
+import restoran.model.osoba.Gost;
 import restoran.model.osoba.Kuvar;
 import restoran.servis.RestoranServis;
 
@@ -24,65 +28,70 @@ import restoran.servis.RestoranServis;
 @RequestMapping("/menadzer")
 public class MenadzerController {
 
-	//private final MenadzerRestoranaServis menadzerRestServis;
-	//private HttpSession httpSession;
-    private RestoranServis restoranServis;   
-	
+	// private final MenadzerRestoranaServis menadzerRestServis;
+	// private HttpSession httpSession;
+	private RestoranServis restoranServis;
+
 	@Autowired
 	public MenadzerController(final RestoranServis restServis) {
-		//this.menadzerRestServis = servis;
-		//this.httpSession = httpSession;
-		this.restoranServis = restServis;	
+		// this.menadzerRestServis = servis;
+		// this.httpSession = httpSession;
+		this.restoranServis = restServis;
 	}
-     
-	@GetMapping (path = "/{id}")
-	public  ResponseEntity<List<Restoran>> list(@PathVariable Long id){
+
+	
+	@PutMapping(path = "/updateRest/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Restoran update(@PathVariable Long id, @Valid @RequestBody Restoran rest) {
+		Optional.ofNullable(restoranServis.findOne(id))
+				.orElseThrow(() -> new ResourceNotFoundException("Resource Not Found!"));
+		rest.setId(id);
+		return restoranServis.save(rest);
+	}
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<List<Restoran>> list(@PathVariable Long id) {
 		List<Restoran> l = restoranServis.findAll();
-	    List<Restoran> lista = new ArrayList<Restoran>();
-		for(int i=0; i<l.size();i++){
+		List<Restoran> lista = new ArrayList<Restoran>();
+		for (int i = 0; i < l.size(); i++) {
 			Restoran r = l.get(i);
-			for(int j=0; j< r.getMenadzeriRestorana().size(); j++){
-                 if(id.equals(r.getMenadzeriRestorana().get(j).getId())){
-                	 lista.add(r);
-                 }
+			for (int j = 0; j < r.getMenadzeriRestorana().size(); j++) {
+				if (id.equals(r.getMenadzeriRestorana().get(j).getId())) {
+					lista.add(r);
+				}
 			}
 		}
-		
-		return new ResponseEntity<>( lista,HttpStatus.OK);
-		
-		}
-	
-	
+
+		return new ResponseEntity<>(lista, HttpStatus.OK);
+
+	}
 
 	@PostMapping(path = "/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void dodajKonobara(@Valid @RequestBody Kuvar cook, Long id) {
-		
+
 		Restoran restaurant = restoranServis.findOne(id);
-	   // restaurant.getKuvari().add(cook);
+		// restaurant.getKuvari().add(cook);
 		restoranServis.save(restaurant);
 	}
-/*
-	@PostMapping(path = "/restaurant/saveBartender")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void saveCook(@Valid @RequestBody Bartender bartender) {
-		bartender.setRegistrated("0");
-		Restaurant restaurant = findRestaurantForRestaurantManager();
-		restaurant.getBartenders().add(bartender);
-		restaurantService.save(restaurant);
-	}
+	/*
+	 * @PostMapping(path = "/restaurant/saveBartender")
+	 * 
+	 * @ResponseStatus(HttpStatus.CREATED) public void
+	 * saveCook(@Valid @RequestBody Bartender bartender) {
+	 * bartender.setRegistrated("0"); Restaurant restaurant =
+	 * findRestaurantForRestaurantManager();
+	 * restaurant.getBartenders().add(bartender);
+	 * restaurantService.save(restaurant); }
+	 * 
+	 * @PostMapping(path = "/restaurant/saveBidder")
+	 * 
+	 * @ResponseStatus(HttpStatus.CREATED) public void
+	 * saveBidder(@Valid @RequestBody Bidder bidder) {
+	 * bidder.setRegistrated("0"); Restaurant restaurant =
+	 * findRestaurantForRestaurantManager();
+	 * restaurant.getBidders().add(bidder); restaurantService.save(restaurant);
+	 * }
+	 */
 
-	@PostMapping(path = "/restaurant/saveBidder")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void saveBidder(@Valid @RequestBody Bidder bidder) {
-		bidder.setRegistrated("0");
-		Restaurant restaurant = findRestaurantForRestaurantManager();
-		restaurant.getBidders().add(bidder);
-		restaurantService.save(restaurant);
-	}
-*/
-
-	
-	}
-
-
+}
--- restoran/src/main/resources/static/menadzerRestorana/menadzerRestoranaController.js
+++ restoran/src/main/resources/static/menadzerRestorana/menadzerRestoranaController.js
@@ -1,30 +1,50 @@
 var app = angular.module('menadzerRestorana.controllers', []);
 
-app.controller('menadzerRestoranaController', ['$scope','menadzerRestoranaService', '$location',
-                                             	function ($scope, menadzerRestoranaService, $location) {
-	
-	       
-	$scope.getLoggedUser = function() {
-		menadzerRestoranaService.getLoggedUser($scope.loggedUser).then(
-			function (response) {
-				$scope.loggedUser = response.data;
-            }	
+app.controller('menadzerRestoranaController', [
+		'$scope',
+		'menadzerRestoranaService',
+		'$location',
+		function($scope, menadzerRestoranaService, $location) {
+
+			$scope.getLoggedUser = function() {
+				menadzerRestoranaService.getLoggedUser().then(
+						function(response) {
+							$scope.loggedUser = response.data;
+							$scope.loggedUser;
+							findRest($scope.loggedUser);
+						})
+			}
+
+			function findRest(loggedUser) {
+				menadzerRestoranaService.list($scope.loggedUser).then(
+						function(response) {
+							$scope.lista = response.data;
+							//$location.path('/menadzerRestorana/restorani');
+						});
+			}
 			
+			$scope.zaIzmenu = function(restoran){
+				menadzerRestoranaService.find(restoran.id).then(
+						function(response){
+							$scope.prikazi = "true";
+							$scope.resttt = response.data;
+							$location.path('/menadzerRestorana/restorani');
+						}
+						)
 			
-		)
-		
-	}
-	
-	$scope.list = function () {            
-		menadzerRestoranaService.list($scope.loggedUser).then(
-				function (response) {
-					$scope.lista = response.data;
-				$location.path('/menadzerRestorana/restorani');
+			}
 			
-				}
+			$scope.update = function() {
+				menadzerRestoranaService.updateRest($scope.resttt).then(
+					function (response) {
+						$scope.prikazi = "false";
+	                    $scope.state = undefined;
+	                    findRest($scope.loggedUser);
+	                    $location.path('/menadzerRestorana/restorani');
+					}
 				);
-	}
-	
-	
-}]);
- 
+			}
+			
+			
+			
+		} ]);
