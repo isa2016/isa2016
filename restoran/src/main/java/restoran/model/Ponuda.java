5f013c4b8ed713a3686779c1fb42c5ae6c6d85c8
Milan <mgvero94@gmail.com>
2017-02-23 01:16:34 +0100

    Objava ponude i ponuda\ (samo model)

---
+++ restoran/src/main/java/restoran/model/Ponuda.java
@@ -0,0 +1,59 @@
+package restoran.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Ponuda {
+
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "Ponuda_ID")
+	private Long id;
+	
+	@ManyToMany
+	@JoinTable(name = "Ponuda_pica", joinColumns = @JoinColumn(name = "Porudzbina_ID"), inverseJoinColumns = @JoinColumn(name = "Pice_ID"))
+	private List<Pice> pice;
+
+	@ManyToMany
+	@JoinTable(name = "Ponuda_hrana", joinColumns = @JoinColumn(name = "Porudzbina_ID"), inverseJoinColumns = @JoinColumn(name = "Jelo_ID"))
+	private List<Jelo> hrana;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<Pice> getPice() {
+		return pice;
+	}
+
+	public void setPice(List<Pice> pice) {
+		this.pice = pice;
+	}
+
+	public List<Jelo> getHrana() {
+		return hrana;
+	}
+
+	public void setHrana(List<Jelo> hrana) {
+		this.hrana = hrana;
+	}
+	
+	
+	
+}
--- restoran/src/main/resources/static/gost/guestController.js
+++ restoran/src/main/resources/static/gost/guestController.js
@@ -12,6 +12,7 @@ app.controller('guestController', [
 					$scope.restorani = response.data;
 				});
 			}
+			
 
 			$scope.getLoggedUser = function() {
 				guestService.getLoggedUser().then(function(response) {
